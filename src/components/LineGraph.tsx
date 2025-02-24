d7609d6bfde3f4e993bebcf29ba8f0ec257ba138
Younes <younes.elhjouji@gmail.com>
2025-02-24 16:54:33 +0000

    hotfix

--- src/components/LineGraph.tsx
+++ src/components/LineGraph.tsx
@@ -18,9 +18,12 @@ export type LineGraphProps = {
 };
 
 const LineGraph: React.FC<LineGraphProps> = ({ data }) => {
+  // Remove the first data point
+  const filteredData = data.slice(1);
+
   return (
     <ResponsiveContainer width="100%" height={80}>
-      <LineChart data={data}>
+      <LineChart data={filteredData}>
         <Line
           type="monotone"
           dataKey="downloads"
